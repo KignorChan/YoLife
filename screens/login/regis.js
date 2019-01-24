6cb59aad60acb909d37cd6d3c97046d760c6fb73
rickzhiqiangzhao <rick.zhiqiang.zhao@gmail.com>
2019-01-24 15:06:55 -0500

    register

--- screens/login/regis.js
+++ screens/login/regis.js
@@ -8,14 +8,24 @@ import {
   KeyboardAvoidingView,
   TouchableHighlight,
   TouchableOpacity,
-  Alert,
-  Button
+  Alert
 } from 'react-native';
-
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as _ from 'lodash';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { Isao,Fumi } from 'react-native-textinput-effects';
+import {
+  Button,
+  Text,
+  Left,
+  Body,
+  Right,
+  Container,
+  Content,
+  Header,
+  Icon,
+} from 'native-base';
 import { Actions } from 'react-native-router-flux';
-import DataUtil from '../../utils/DataUtil';
+import DataUtil from  '../../utils/DataUtil';
 import {Toast} from 'teaset';
 
 export default class RegisterView extends React.Component {
@@ -54,32 +64,97 @@ export default class RegisterView extends React.Component {
       //this.setState({ message: 'Password does not match', confirmed: false });
       Toast.sad('Password does not match');
       return ;
-    } 
+    } else {
+      this.setState({ message: 'Loading...', loading: true, confirmed: true });
+      FireBase.signup(this.state.username, this.state.password)
+        .then(result => {
+          FireBase.updateUserType(result.uid, this.state.dropdownText);
+          //if(result.emailVerified){
+            FireBase.login(false, {
+              email: this.state.username,
+              password: this.state.password,
+            })
+              .then(r1 => {
+                FireBase.getDatabase().ref('users/'+result.uid).on('value',snapshot=>{
+                  var user = snapshot.val();
+                  Authentication.saveItem('userId', result.uid);
+                  Authentication.saveItem('userEmail', this.state.username);
+                  FireBase.currentUser.userType = user.userType;
+                });
+  
+                Actions.push('mainPage');
+              })
+              .catch(e => {
+                console.log(e.message);
+                this.setState({ message: e.message, loading: false });
+              });
+          // }else{
+          //   Actions.push('emailVerification',{from:'registerView'})
+          // }
+          
+        })
+        .catch(error => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          this.setState({ message: error.message, loading: false });
+        });
+    }
   };
 
   componentDidMount() {
     this.debouncedSetState = _.debounce(this.setState, 300);
   }
 
+  renderUserTypeSelection(){
+    Alert.alert(
+      'Account Type',
+      'Please choose an account type:',
+      [
+        {text: 'customer', onPress: () => this.setState({dropdownText: 'customer'})},
+        {text: 'business', onPress: () => this.setState({dropdownText: 'business'})},
+        {text: 'Cancel', onPress: () => console.log('cancel pressed!')},
+      ],
+      { cancelable: false }
+    )
+
+    // return (
+    //   <DropdownMenu
+    //     style={{flex: 1}}
+    //     bgColor={'white'}
+    //     tintColor={'#666666'}
+    //     activityTintColor={'green'}
+    //     // arrowImg={}      
+    //     // checkImage={}   
+    //     // optionTextStyle={{color: '#333333'}}
+    //     // titleStyle={{color: '#333333'}} 
+    //     maxHeight={300} 
+    //     handler={(selection, row) => this.setState({dropdownText: this.state.userType[selection][row]})}
+    //     data={this.state.userType}
+    //   >
+    //   </DropdownMenu>
+    // );
+  }
+
   render() {
     return (
-      <View>
-
-          <View style={styles.left}>
+      <Container>
+        <Header>
+          <Left>
             <Button transparent onPress={() => Actions.pop()}>
-              
+              <Icon name="arrow-back" style={{ color: 'black' }} />
             </Button>
-          </View>
-          <View style={styles.body}>
+          </Left>
+          <Body>
             <Text>Register</Text>
-          </View>
-
-
-        <View style={ styles.content }>
+          </Body>
+          <Right />
+        </Header>
+        <Content style={{ backgroundColor: 'white' }}>
           <View style={{ marginLeft: 20, marginRight: 20, marginTop: 30 }}>
             <Fumi
               label="Email"
-              iconClass={Ionicons}
+              iconClass={FontAwesomeIcon}
               iconName={'envelope'}
               iconColor={'#f95a25'}
               iconSize={20}
@@ -90,7 +165,7 @@ export default class RegisterView extends React.Component {
             />
             <Fumi
               label="Password"
-              iconClass={Ionicons}
+              iconClass={FontAwesomeIcon}
               iconName={'key'}
               iconColor={'#f95a25'}
               iconSize={20}
@@ -102,7 +177,7 @@ export default class RegisterView extends React.Component {
             />
             <Fumi
               label="Password Confirm"
-              iconClass={Ionicons}
+              iconClass={FontAwesomeIcon}
               iconName={'key'}
               iconColor={'#f95a25'}
               iconSize={20}
@@ -114,7 +189,7 @@ export default class RegisterView extends React.Component {
             />
 
             <Fumi
-                iconClass={Ionicons}
+                iconClass={FontAwesomeIcon}
                 iconName={'phone'}
                 iconColor={'#f95a25'}
                 iconSize={20}
@@ -123,6 +198,15 @@ export default class RegisterView extends React.Component {
                   this.debouncedSetState({ mobile: value })
                 }
             />
+
+              {/* <TouchableOpacity onPress={this.renderUserTypeSelection.bind(this)}>
+              <View style={{flex: 1, paddingTop:20, flexDirection:'row'}}>
+  
+                <Text style={{fontSize:16, fontWeight:'bold'}}>Sign up as:</Text>
+                <Text style={{fontSize:16, marginLeft:20}}>{this.state.dropdownText}</Text>
+                </View>
+
+              </TouchableOpacity> */}
 
 
             {this.state.message.length > 0 ? (
@@ -144,19 +228,8 @@ export default class RegisterView extends React.Component {
               <Text style={{ color: 'white' }}>Register</Text>
             </Button>
           </View>
-        </View>
-      </View>
+        </Content>
+      </Container>
     );
   }
 }
-const styles = StyleSheet.create({
-  left: {
-   
-  },
-  body: {
-   
-  },
-  content: {
-   
-  },
-})
